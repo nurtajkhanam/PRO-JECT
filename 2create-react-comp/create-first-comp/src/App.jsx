4641a46ff17f07cb1f1647f05fa6b1f9515ec5db
nurtaj <nurtajkhanam777@gmail.com>
2025-02-12 11:40:52 +0600

    Added

---
+++ 2create-react-comp/create-first-comp/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import React from "react";
+import KgButton from "./KgButton";
+import Random from "./Random";
+
+const App = () => {
+  return (
+    <div>
+      <KgButton />
+      <Random />
+      <Random />
+      <Random />
+      <Random />
+    </div>
+  );
+};
+
+export default App;
---
+++ 2create-react-comp/create-first-comp/src/KgButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+let name = () => {
+  return "Nur Taj";
+};
+
+const KgButton = () => {
+  return <div> I'm a learner named : {name()} </div>;
+};
+
+export default KgButton;
